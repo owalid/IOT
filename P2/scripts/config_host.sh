6ded583a54c89d870e73b36b23e8e2ba6845ccb0
kibatche <kibatche@kibatche>
2023-05-09 22:41:13 +0200

    Working P2

--- P2/scripts/config_host.sh
+++ P2/scripts/config_host.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-IP=""
+IP="192.168.56.0/8"
 
 if [ ! -f "/etc/vbox/networks.conf" ]; then
-    sudo echo "* 192.168.56.0/8" > /etc/vbox/networks.conf
+    sudo echo "* $IP" > /etc/vbox/networks.conf
     if [ ! $? -eq 0 ]; then
         echo "Something did not work during the creation of the configuration file for virtualbox network. Try again"
     else
         echo "Done."
     fi
 else
-    echo "File already exists"
+    echo "/etc/vbox/networks.conf File already exists"
 fi
--- P2/scripts/config_k3s.sh
+++ P2/scripts/config_k3s.sh
@@ -1,64 +1,117 @@
 #!/bin/bash
 
-# export DEBIAN_FRONTEND=noninteractive
+BOLD_RED="\033[1;31m"
+BOLD_BLUE="\033[1;34m"
+RESET="\033[0m"
+
+function message
+{
+    echo -e "${BOLD_BLUE}$1${RESET}"
+}
+
+function error
+{
+    echo -e "${BOLD_RED}$1${RESET}"
+}
 
 function check_exit_code
 {
     if [ ! $? -eq 0 ]; then
-    echo "Installation of "$1" failed. The program will exit."
+    error "Installation of "$1" failed. The program will exit."
     exit 1
     else
-        echo ""$1" installed, cool !"
+        message ""$1" installed, cool !"
     fi
 }
 
-echo "Update debian database and Add curl to it."
-sudo apt update
-sudo apt install curl gpg -y
-which curl
-if [ ! $? -eq 0 ]; then
-    echo "Curl could not be installed"
-    exit 1
-fi
-echo "Installing k3s..."
-curl -sfL https://get.k3s.io | sh -
-if [ ! -d /home/vagrant/.kube ];then
-    echo ".kube directory does not exist. We will create it."
-    mkdir /home/vagrant/.kube
-fi
-if [ ! -f  /home/vagrant/.kube/config ];then
-    echo "config file does not exist. We will create it."
-    kubectl config view --raw >> /home/vagrant/.kube/config
-fi
-echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
-sudo k3s kubectl get node
-check_exit_code "k3s"
-echo "Installing helm package manager for kubernetes"
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
-which helm
-check_exit_code "helm"
-echo "Generating a certicates for secure http..."
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
-echo "Add secret, key and cert to k3s"
-kubectl create secret tls tls-secret --key tls.key --cert tls.crt
-echo "Installing nginx ingress-controller as reverse proxy for applications..."
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm install --kubeconfig /home/vagrant/.kube/config ingress-nginx-app123 ingress-nginx/ingress-nginx \
---set controller.defaultTLS.cert=tls.crt --set controller.defaultTLS.key=tls.key --set controller.defaultTLS.secret=tls-secret \
---set controller.service.loadBalancerIP=192.168.56.110
-helm status ingress-nginx
-check_exit_code "ingress-nginx"
-wget https://github.com/paulbouwer/hello-kubernetes/archive/refs/tags/v1.10.1.tar.gz
-tar -xvzf v1.10.1.tar.gz
-cd hello-kubernetes-1.10.1/deploy/helm/
-helm install --kubeconfig /home/vagrant/.kube/config --create-namespace --namespace hello-kubernetes ingress-nginx-app123 ./hello-kubernetes \
-  --set ingress.configured=true --set ingress.pathPrefix=custom-message \
-  --set service.type=ClusterIP \
-  --set message="Hello from App3" \
-  --set controller.replicaCount=3
+function add_ressources
+{
+    message "Update debian database and add curl and gpg to it."
+    sudo apt update
+    sudo apt install curl gpg -y
+    which curl
+    check_exit_code "curl"
+    which gpg
+    check_exit_code "gpg"
+    message "Installing k3s..."
+    curl -sfL https://get.k3s.io | sh -s - server --node-ip=192.168.56.110
+    if [ ! -d /home/vagrant/.kube ];then
+        message ".kube directory does not exist. We will create it."
+        mkdir /home/vagrant/.kube
+    fi
+    if [ ! -f  /home/vagrant/.kube/config ];then
+        message "config file does not exist. We will create it."
+        kubectl config view --raw >> /home/vagrant/.kube/config
+    fi
+    echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+    source /home/vagrant/.bashrc
+    chmod 644 /home/vagrant/.kube/config
+    sudo k3s kubectl get node
+    check_exit_code "k3s"
+    message "Installing helm package manager for kubernetes"
+    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    sudo apt-get install apt-transport-https --yes
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    sudo apt-get update
+    sudo apt-get install helm
+    which helm
+    check_exit_code "helm"
+}
+
+function add_helm_repos
+{
+    message "Add repos for nginx ingress controller and hello-kubernetes app"
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo add opsmx https://helmcharts.opsmx.com/
+    helm repo update
+}
+
+function add_nginx_ingress_controller
+{
+    message "Installing nginx ingress-controller as reverse proxy for applications..."
+    sudo helm install --wait --kubeconfig /home/vagrant/.kube/config ingress-nginx-hello-kubernetes ingress-nginx/ingress-nginx
+    helm status ingress-nginx-hello-kubernetes
+}
+
+function add_hello_kubernetes
+{
+    message "Installing "$1" instance of hello kubernetes"
+    sudo helm install --wait --kubeconfig /home/vagrant/.kube/config "$1" opsmx/hello-kubernetes --version 1.0.3 \
+    --set ingress.configured=true \
+    --set ingress.rewritePath=/ \
+    --set service.type=ClusterIP \
+    --set message="Hello from "$1"" \
+    --set deployment.replicaCount=$2
+}
+
+function tests
+{
+    sleep 1
+    message "Getting pods"
+    kubectl get po -o wide
+    sleep 1
+    message "Getting services"
+    kubectl get svc
+    slepp 1
+    message "Get everything"
+    kubectl get all
+    sleep 1
+    message "Test with App1"
+    curl -H "Host: app1.com" 192.168.56.110
+    sleep 1
+    message "Test with App2"
+    curl -H "Host: app2.com" 192.168.56.110
+    sleep 1
+    message "Test with App3"
+    curl 192.168.56.110
+    message "You can know test it on the host machine!"
+}
+
+add_ressources
+add_helm_repos
+add_nginx_ingress_controller
+add_hello_kubernetes app1 1
+add_hello_kubernetes app2 3
+add_hello_kubernetes app3 1
+kubectl apply -n default -f /controllers/ingress/ingress_hello-kubernetes.yaml
+tests

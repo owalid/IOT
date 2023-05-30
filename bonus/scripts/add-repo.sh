bb586d1478b1f46e564b827aa806a43206048d47
owalid <othmane.el.ayadi@gmail.com>
2023-05-30 23:40:17 +0200

    update ok gitlab
    improve add-repo bash script

---
+++ bonus/scripts/add-repo.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+BOLD_RED="\033[1;31m"
+BOLD_BLUE="\033[1;34m"
+RESET="\033[0m"
+function message
+{
+    echo -e "${BOLD_BLUE}$1${RESET}"
+}
+
+function error
+{
+    echo -e "${BOLD_RED}$1${RESET}"
+}
+
+function check_exit_code
+{
+    if [ ! $? -eq 0 ]; then
+        error "Installation of "$1" failed. The program will exit."
+        exit 1
+    else
+        message ""$1" installed, cool !"
+    fi
+}
+
+
+function check_empty_var
+{
+    if [ -z "$2" ]
+    then
+        error "$1 is empty"
+        error "Usage ./add-repo.sh <GITLAB_USER> <GITLAB_REPO_NAME> <ARGOCD_APP_NAME>"
+        exit 1
+    fi
+}
+
+
+CLUSTER_IP=$(kubectl get service/gitlab-service -n gitlab -o go-template='{{(.spec.clusterIPs)}}' | tr -d '[]')
+GITLAB_USER=$1
+GITLAB_REPO_NAME=$2
+ARGOCD_APP_NAME=$3
+GITLAB_END_URL="$GITLAB_USER/$GITLAB_REPO_NAME.git"
+check_empty_var $CLUSTER_IP "CLUSTER_IP"
+check_empty_var $GITLAB_USER "Gitlab user"
+check_empty_var $GITLAB_REPO_NAME "Gitlab repository name"
+check_empty_var $ARGOCD_APP_NAME "Name of argocd application to add"
+
+message "Login to argocd"
+argocd login argocd.local:8080 --port-forward-namespace argocd --insecure --plaintext
+
+message "Add repository http://$CLUSTER_IP/$GITLAB_END_URL with application name: $3 in argocd"
+argocd app create $3 --repo http://$CLUSTER_IP/$GITLAB_END_URL --path . --dest-server https://kubernetes.default.svc --dest-namespace dev --sync-policy automated --insecure --plaintext
+
+message "Sync $3 application name in argocd"
+argocd app sync $3 --port-forward-namespace argocd --insecure --plaintext
--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -3,7 +3,6 @@
 BOLD_RED="\033[1;31m"
 BOLD_BLUE="\033[1;34m"
 RESET="\033[0m"
-ARGOCD_OPTS="--port-forward-namespace argocd --insecure --plaintext"
 function message
 {
     echo -e "${BOLD_BLUE}$1${RESET}"
@@ -24,9 +23,19 @@ function check_exit_code
     fi
 }
 
+message "Create namespace gitlab"
 kubectl create namespace gitlab
 kubectl get ns | grep 'gitlab'
 check_exit_code "namespace gitlab"
 kubectl apply -f ../controllers/gitlab/gitlab-deployment.yaml -n gitlab
 kubectl apply -f ../controllers/gitlab/gitlab-svc.yaml -n gitlab
+message "Wait pod to be ready"
+kubectl wait -n gitlab --for=condition=ready --timeout=600s pod -l app=gitlab-app
+kubectl get all -n gitlab
+message "Sleep 60sec to be sure that the gitlab is fully available"
+sleep 60
+message "Port forward"
 k3d cluster edit p3-iot --port-add "7777:30082@loadbalancer"
+message "Installation successful you can now connect to the gitlab at: http://127.0.0.1:7777"
+message "There is the password of root user of gitlab:"
+kubectl exec -it deployment/gitlab-deployment -n gitlab -- grep 'Password:' /etc/gitlab/initial_root_password
